176955dc1007c85d4ef2fd8b92bf5b740a070df3
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-02-19 22:47:48 -0300

    feat: estrutura inicial

---
+++ src/main/java/leandrodev/MoneyScope/MoneyScopeApplication.java
@@ -0,0 +1,13 @@
+package leandrodev.MoneyScope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyScopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leandrodev/MoneyScope/MoneyScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package leandrodev.MoneyScope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
